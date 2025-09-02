80dfb3d368b7dd411d50b878bae3b69b9284838c
diegobarr <d1390xd.v@gmail.com>
2025-09-01 19:07:20 -0600

    Commit conexion inicial

--- gestion_cursos_online.py
+++ gestion_cursos_online.py
@@ -1,1 +1,20 @@
-print("Programa :D")
+class Usuario: #para guardar al instructor y al estudiante
+    def __init__(self, nombre, correo, telefono):
+        self.nombre = nombre
+        self.correo = correo
+        self.telefono = telefono
+
+    def mostrar_datos(self):
+        pass
+
+class Estudiante(Usuario):
+    def __init__(self, nombre, correo, telefono, carnet):
+        super().__init__(nombre, correo, telefono)
+        self.carnet = carnet
+
+    def mostrar_datos(self):
+        return f"Carnet: {self.carnet} | Nombre: {self.nombre} | Correo: {self.correo} Telefono: {self.telefono}"
+
+
+
+
