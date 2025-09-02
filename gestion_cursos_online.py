b5b073d373949a17d3a67a45f6e0fa2f973b0a61
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-09-01 19:30:22 -0600

    creacion de clases admin e instructor

--- gestion_cursos_online.py
+++ gestion_cursos_online.py
@@ -11,10 +11,27 @@ class Estudiante(Usuario):
     def __init__(self, nombre, correo, telefono, carnet):
         super().__init__(nombre, correo, telefono)
         self.carnet = carnet
+        self.curso = {}
 
     def mostrar_datos(self):
         return f"Carnet: {self.carnet} | Nombre: {self.nombre} | Correo: {self.correo} telefono: {self.telefono}"
 
+class instructor(Usuario):
+    def __init__(self,nombre,correo,telefono,codigo,profesion):
+        super().__init__(nombre,correo,telefono)
+        self.__codigo = codigo
+        self.profesion = profesion
+        self.estudiantes = {}
 
+    def ver_codigo(self):
+        return self.__codigo
 
+    def mostrar_datos(self):
+        return f"Instructor-Nombre: {self.nombre} | Profesion: {self.profesion} | Correo: {self.correo} | telefono: {self.telefono}"
 
+class Administrador(Usuario):
+    def __init__(self,nombre,correo):
+        super().__init__(nombre,correo)
+
+    def crear_curso(self,curso,tipo):
+        pass
