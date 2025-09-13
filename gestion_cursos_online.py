76f30008ad483ef7f42fc1cd7247f0c42d47df2d
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-09-13 15:50:18 -0600

    estructura de menu

--- gestion_cursos_online.py
+++ gestion_cursos_online.py
@@ -1,35 +1,21 @@
 class Usuario:  # Clase base para Instructor y Estudiante
     def __init__(self, nombre, correo, telefono):
-        self._nombre = nombre
-        self._correo = correo
-        self._telefono = telefono
+        self.nombre = nombre
+        self.correo = correo
+        self.telefono = telefono
 
     def mostrar_datos(self):
         pass
 
-    @property
-    def nombre(self):
-        return self._nombre
-
-    @property
-    def correo(self):
-        return self._correo
-
-    @property
-    def telefono(self):
-        return self._telefono
-
-
 class Curso:
-    def __init__(self, nombre_curso, codigo_curso):
-        self._nombre_curso = nombre_curso
+    def __init__(self, nombre_curso, codigo_curso, evaluacion, tarea):
+        self.nombre_curso = nombre_curso
         self._codigo_curso = codigo_curso
         self.instructor = None
         self.estudiantes = []  # almacenar ids o referencias a estudiantes
+        self.tarea = tarea
+        self.evaluacion = evaluacion
 
-    @property
-    def nombre_curso(self):
-        return self._nombre_curso
 
     @property
     def codigo_curso(self):
@@ -42,8 +28,7 @@ class Curso:
         self.estudiantes.append(estudiante)
 
     def mostrar_datos(self):
-        return f"Curso: {self._nombre_curso} | Código: {self._codigo_curso} | Instructor: {self._instructor.nombre if self._instructor else 'Sin asignar'}"
-
+        return f"Curso: {self.nombre_curso} | Código: {self._codigo_curso} | Instructor: {self._instructor.nombre if self._instructor else 'Sin asignar'}"
 
 class Estudiante(Usuario):
     def __init__(self, nombre, correo, telefono, carnet):
@@ -72,14 +57,14 @@ class Instructor(Usuario):
         super().__init__(nombre, correo, telefono)
         self._codigo = codigo
         self._profesion = profesion
-        self._cursos_impartidos = set()
+        self._cursos_impartidos = []
 
     @property
     def codigo(self):
         return self._codigo
 
     def asignar_curso(self, curso):
-        self._cursos_impartidos.add(curso)
+        self._cursos_impartidos.append(curso)
         curso.asignar_instructor(self)
 
     def mostrar_datos(self):
@@ -90,53 +75,149 @@ class Instructor(Usuario):
             cursos = ", ".join(nombres_cursos)
         else:
             cursos = "Ninguno"
-
         return f"Instructor | Código: {self._codigo} | Profesión: {self._profesion} | {super().mostrar_datos()} | Cursos: {cursos}"
 
-class Evaluacion:
-    def __init__(self, nombre, punteo):
-        self.nombre = nombre
-        self._punteo = punteo
-        self._calificaciones = {}
+class Administrador(Usuario):
+    def __init__(self):
+        super().__init__("Admin", None, 11110000)
+        self._codigo_ingreso = "администратор007"
+        self._cursos_creados = []
 
     @property
-    def punteo(self):
-        return self._punteo
+    def codigo_ingreso(self):
+        return self._codigo_ingreso
 
-    @property
-    def calificaciones(self):
-        return self._calificaciones
+    def crear_curso(self, nombre_curso, codigo_curso, evaluacion=None, tarea=None):
+        curso = Curso(nombre_curso, codigo_curso, evaluacion, tarea)
+        self._cursos_creados.append(curso)
+        #return curso
 
-    def registrar_calificacion(self, estudiante, nota):
-        self._calificaciones[estudiante._carnet] = nota
+    def asignar_curso_a_instructor(self, curso, instructor):
+        instructor.asignar_curso(curso)
 
-    def ver_calificaciones(self):
-        if self._calificaciones:
-            for carnet, nota in self._calificaciones.items():
-                print(f"Carnet: {carnet} | Nota: {nota}")
+admin=Administrador()
+
+print("----🛜 PORTAL CURSOS ONLINE 🛜----")
+
+print("Iniciar sesión\n")
+print("1. Administrador")
+print("2. Instructor")
+print("3. Estudiante")
+print("4. Salir")
+
+opcion1=input("\nSeleccione su tipo de usuario")
+
+match opcion1:
+    case "1":
+        codigo=input("Ingrese su codigo de admin: ")
+
+        if codigo==admin.codigo_ingreso:
+            print("--Portal del ADMIN 🤑🤑--\n")
+            print("1. Crear curso")
+            print("2. Asignar curso a instructor")
+            print("3. Salir")
+
+            opcion2=input("\nSeleccione lo que desee: ")
+
+            match opcion2:
+                case "1":
+                    print("Aqui se va a crear curso")
+                case "2":
+                    print("Aqui se va a asignar curso")
+                case "3":
+                    #break
         else:
-            print("No hay calificaciones registradas.")
+            print("Codio incorrecto ✖️")
 
-class Examen(Evaluacion):
-    def __init__(self, nombre, ponderacion, duracion):
-        super().__init__(nombre, ponderacion)
-        self._duracion = duracion
+    case "2":
+        print("--Portal Instructor👨‍🏫👩‍🏫--\n")
+        print("1. Registrar usuario")
+        print("2. Acceder al portal")
+        print("3. Salir")
 
-    @property
-    def duracion(self):
-        return self._duracion
+        opcion3=input("\nInicie sesión")
 
-    def mostrar_info(self):
-        return f"Examen: {self.nombre} | Ponderación: {self._punteo}% | Duración: {self._duracion} min"
+        match opcion3:
+            case "1":
+                print("Aqui se va a registrar usted")
 
-class Tarea(Evaluacion):
-    def __init__(self, nombre, ponderacion, fecha_entrega):
-        super().__init__(nombre, ponderacion)
-        self._fecha_entrega = fecha_entrega
+            case "2":
+                print("Aqui usted ya esta dentro del portal")
 
-    @property
-    def fecha_entrega(self):
-        return self._fecha_entrega
+                profesor=input("Ingrese su código de profesor")
 
-    def mostrar_info(self):
-        return f"Tarea: {self.nombre} | Ponderación: {self._punteo}% | Fecha de entrega: {self._fecha_entrega}"
+                if profesor in profesores:
+                    print(f"Bienvenido {profesor}")
+
+                    print("1. Añadir evaluacion a su curso")
+                    print("2. Añadir tarea a su curso")
+                    print("3. Añadir notas")
+                    print("4. Salir")
+
+                    option4=input("\nSeleccione una opción: ")
+
+                    match option4:
+                        case "1":
+                            print("añadir evaluacion ")
+
+                        case "2":
+                            print("añadir una tarea")
+
+                        case "3":
+                            print("añadir notas")
+
+
+                        case "4":
+                            print("Salir")
+                else:
+                    print("Codigo de profesor incorrecto✖️")
+
+    case "3":
+        print("--Portal Estudiante 🎓--\n")
+        print("1. Registrarse")
+        print("2. Acceder al portal")
+        print("3. Salir")
+
+        opcion5 = input("\nSeleccione lo que desee: ")
+
+        match opcion5:
+            case "1":
+                print("Aquí se va a registrar el estudiante")
+
+            case "2":
+                estudiante = input("Ingrese su código de estudiante: ")
+
+                if estudiante in estudiantes:
+                    print(f"Bienvenido {estudiante}")
+
+                    print("1. Inscibirse a un curso")
+                    print("2. ver cursos inscritos")
+                    print("3. ver tareas")
+                    print("4. ver notas")
+                    print("5. Salir")
+
+                    opcion6 = input("\nSeleccione una opcion: ")
+                    match opcion6:
+                        case "1":
+                            print("inscribirse a un curso")
+
+                        case "2":
+                            print("Mostrar cursos")
+
+                        case "3":
+                            print("Mostrar tareas")
+
+                        case "4":
+                            print("Mostrar notas")
+
+                        case "5":
+                            print("Salir")
+                else:
+                    print("Codigo de estudiante incorrecto")
+
+            case "3":
+                print(" salir")
+    case "4":
+        print("Saliendo.....")
+
+
