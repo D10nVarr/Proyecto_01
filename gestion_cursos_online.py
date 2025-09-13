8c8f35299b95f29f6fa15089fa1e5bb71813192b
diegobarr <d1390xd.v@gmail.com>
2025-09-13 13:56:20 -0600

    Reorganización de clases, curso, instructor, junto con implemendo de nuevos métodos de muestreo

--- gestion_cursos_online.py
+++ gestion_cursos_online.py
@@ -1,45 +1,94 @@
-class Usuario: #para guardar al instructor y al estudiante
+class Usuario:  # Clase base para Instructor y Estudiante
     def __init__(self, nombre, correo, telefono):
-        self.nombre = nombre
-        self.correo = correo
-        self.telefono = telefono
+        self._nombre = nombre
+        self._correo = correo
+        self._telefono = telefono
 
     def mostrar_datos(self):
         pass
 
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @property
+    def correo(self):
+        return self._correo
+
+    @property
+    def telefono(self):
+        return self._telefono
+
+
 class Curso:
     def __init__(self, nombre_curso, codigo_curso):
-        self.nombre_curso = nombre_curso
-        self.codigo_curso = codigo_curso
+        self._nombre_curso = nombre_curso
+        self._codigo_curso = codigo_curso
+        self.instructor = None
+        self.estudiantes = []  # almacenar ids o referencias a estudiantes
+
+    @property
+    def nombre_curso(self):
+        return self._nombre_curso
+
+    @property
+    def codigo_curso(self):
+        return self._codigo_curso
+
+    def asignar_instructor(self, instructor):
+        self._instructor = instructor
+
+    def inscribir_estudiante(self, estudiante):
+        self.estudiantes.append(estudiante)
+
+    def mostrar_datos(self):
+        return f"Curso: {self._nombre_curso} | Código: {self._codigo_curso} | Instructor: {self._instructor.nombre if self._instructor else 'Sin asignar'}"
+
 
 class Estudiante(Usuario):
     def __init__(self, nombre, correo, telefono, carnet):
         super().__init__(nombre, correo, telefono)
-        self.carnet = carnet
-        self.curso = {}
+        self._carnet = carnet
+        self.cursos_inscritos =[]
+
+    def inscribirse(self, curso):
+        self.cursos_inscritos.append(curso)
+        curso.inscribir_estudiante(self)
 
     def mostrar_datos(self):
-        return f"Carnet: {self.carnet} | Nombre: {self.nombre} | Correo: {self.correo} | Telefono: {self.telefono}"
+        if self.cursos_inscritos:
+            nombres_cursos = []
+            for curso in self.cursos_inscritos:
+                nombres_cursos.append(curso.nombre_curso)
+            cursos = ", ".join(nombres_cursos)
+        else:
+            cursos = "Ninguno"
+
+        return f"Estudiante | Carnet: {self._carnet} | {super().mostrar_datos()} | Cursos: {cursos}"
+
 
 class Instructor(Usuario):
-    def __init__(self,nombre,correo,telefono,codigo,profesion, curso):
-        super().__init__(nombre,correo,telefono)
-        self.__codigo = codigo
-        self.profesion = profesion
-        self.estudiantes = {}
-        self.curso = curso
+    def __init__(self, nombre, correo, telefono, codigo, profesion):
+        super().__init__(nombre, correo, telefono)
+        self._codigo = codigo
+        self._profesion = profesion
+        self._cursos_impartidos = set()
 
-    def ver_codigo(self):
-        return self.__codigo
+    @property
+    def codigo(self):
+        return self._codigo
 
-    def crear_curso(self):
-        pass
+    def asignar_curso(self, curso):
+        self._cursos_impartidos.add(curso)
+        curso.asignar_instructor(self)
 
     def mostrar_datos(self):
-        return f"Instructor: {self.nombre} | Código: {self.__codigo} | Profesion: {self.profesion} | Correo: {self.correo} | Telefono: {self.telefono} | Curso: {self.curso.nombre_curso}  {self.curso.codigo_curso}"
+        if self._cursos_impartidos:
+            nombres_cursos = []
+            for curso in self._cursos_impartidos:
+                nombres_cursos.append(curso.nombre_curso)
+            cursos = ", ".join(nombres_cursos)
+        else:
+            cursos = "Ninguno"
 
-curso = Curso("Mate","CR-001")
-profe = Instructor("Diego", "diego@profe.com", "31232566", "777","Ingeniero",curso)
-
-print(profe.mostrar_datos())
-
+        return f"Instructor | Código: {self._codigo} | Profesión: {self._profesion} | {super().mostrar_datos()} | Cursos: {cursos}"
